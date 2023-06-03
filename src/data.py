a97bf8996b51ef8e8a4e13d25ecfd6659b662fd7
Victoria Kashpur <vikki0011@gmail.com>
2023-06-03 22:50:07 +1000

    fix get_data and add sort_data

--- src/data.py
+++ src/data.py
@@ -2,6 +2,15 @@ import json
 
 
 def get_data():
-    with open('operations.json', 'r', encoding='utf-8') as file:
-        data = json.loads(file.read())
-    return data[-1:-5:-1]
+    with open('operations.json', encoding='utf-8') as file:
+        all_data = json.loads(file.read())
+        return all_data
+
+
+def sort_data(data):
+    full_data = []
+    for i in data:
+        if 'state' in i:
+            if i['state'] == "EXECUTED":
+                full_data.append(i)
+    return sorted(full_data, key=lambda k: k['date'].split('.'), reverse=True)
